53f0c0922d4bc6761203efcded469d0687d09e04
fregnani123 <fabianofregnani@hotmail.com>
2023-08-07 22:06:11 -0300

    estudos

--- server.js
+++ server.js
@@ -6,7 +6,19 @@ const mongoose = require('mongoose');
 const routes = require('./routes');
 const path = require('path');
 
+
+
+mongoose.connect(process.env.connectionsMongoDB
+).then(()=>{
+    console.log('Conectado á base de dados.');
+    app.emit('pronto');
+}).catch((e)=> {
+    console.log(e);
+})
+
 app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.use(express.urlencoded({ extended: true }));
 
 app.set('views', path.resolve(__dirname, 'public'));
 app.set('view engine', 'ejs');
@@ -15,6 +27,8 @@ app.get('/', (req, res) => {
     res.render(path.join(__dirname, 'public', 'index.ejs'));
 });
 
+app.on('pronto', ()=> {
 app.listen(process.env.PORT, () => {
     console.log(`Servidor rodando em http://localhost:${process.env.PORT}`);
 });
+})
