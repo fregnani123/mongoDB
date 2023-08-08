a783f38d7994394592db63507a29647559c35e3c
fregnani123 <fabianofregnani@hotmail.com>
2023-08-07 21:16:15 -0300

    enviar

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
-require('dotenv').config();
+require('dotenv').config({ path: 'variaveis.env' });
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
+const routes = require('./routes');
+const path = require('path');
 
+app.use(express.static(path.resolve(__dirname, 'public')));
 
+app.set('views', path.resolve(__dirname, 'public'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render(path.join(__dirname, 'public', 'index.ejs'));
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${process.env.PORT}`);
+});
